cafa0572e66468670dc3c6503a604302630caa9d
Aayushpatil32 <patilaayush3232@gmail.com>
2025-06-12 00:03:26 +0530

    improvement in users table

--- app/users/models.py
+++ app/users/models.py
@@ -1,20 +1,35 @@
 from app import db
 
 class User(db.Model):
-    __tablename__ = 'user_activities'
-
+    __tablename__ = 'users_table'
+    
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    contact_no = db.Column(db.BigInteger, unique=True, nullable=False)
+    address = db.Column(db.String(300), unique=True, nullable=False)  # Changed from Text to String
+    state = db.Column(db.String(70), nullable=False)
+    country = db.Column(db.String(30), nullable=False)
+    city = db.Column(db.String(70), nullable=False)
 
     def to_dict(self):
         return {
-            "id" : self.id,
-            "username" : self.username,
-            "email" : self.email
-                }
+            "id": self.id,
+            "username": self.username,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+            "date_of_birth": str(self.date_of_birth),
+            "contact_no": self.contact_no,
+            "address": self.address,
+            "state": self.state,
+            "country": self.country,
+            "city": self.city
+        }
 
     def __repr__(self):
-        return f'<User {self.username}>'
-    
-    
+        return f'<User {self.id}>'
